5432f378457a2645db471d689c820c02b8c8de98
mpilonhlerdebe <mpilonhleradebe@outlook.com>
2025-05-15 19:36:46 +0200

    minor HelpLine

---
+++ frontend/assets/componets/Helpline.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Helpline = () => {
+  return (
+    <View style = {styles.container}>
+          <Text>Helpline</Text>
+    </View>
+  )
+}
+
+export default Helpline
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
