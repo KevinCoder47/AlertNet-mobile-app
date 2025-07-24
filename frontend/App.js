1a69889c316344b3edde933fc6c4289a1d2304a1
kevincoder47 <serakalalakevin@gmail.com>
2025-07-24 23:00:45 +0200

    added TestSOS Page in SafetyResources

--- frontend/App.js
+++ frontend/App.js
@@ -2,8 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { ThemeProvider, useTheme } from './assets/contexts/ColorContext';
 import AppNavigator from './assets/navigation/AppNavigator'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SOS from './assets/screens/SOS'
+import QrCode from './assets/screens/QrCode';
 
-
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
