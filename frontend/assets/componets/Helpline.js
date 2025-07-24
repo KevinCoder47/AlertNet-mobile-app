1a75705a1acc460b7bcabac5baf2e673ea8b6800
0502081297082 <siphephile222@gmail.com>
2025-07-24 12:51:09 +0200

    Helpline page

--- frontend/assets/componets/Helpline.js
+++ frontend/assets/componets/Helpline.js
@@ -1,18 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View , Dimensions} from 'react-native'
 import React from 'react'
+
+
+
 
 const Helpline = () => {
   return (
-    <View style = {styles.container}>
-          <Text>Helpline</Text>
-    </View>
-  )
+    <View style={styles.container}>
+      <Text style = {styles.title} > HELPLINE</Text>
+      <Text style = {styles.subtitle} >Stay calm - you not alone , help is available.</Text>
+      </View>
+
+    
+     
+  );
 }
+
 
 export default Helpline
 
 const styles = StyleSheet.create({
     container: {
+      flex: 1 ,
+      backgroundColor : "black",
+      position: "absolute",
+      bottom: 120,
+      height: 200,
+      width:350,
+      alignSelf: "center",
+      zIndex: 10,
+      padding: 10,
+      borderRadius: 10,
 
-    }
-})
+
+  },
+  title: {
+    fontSize:16,
+    fontWeight: "bold",
+    fontFamily: "Poppins",
+    color: "#ffffff"
+},
+  subtitle: {
+    fontFamily: "Poppins",
+    fontSize: 12 ,
+    fontWeight: "300",
+    color:"lightgray"
+
+
+  }
+
+
+  }
+)
